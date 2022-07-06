b752ac731d70e0fc60276bdc305f5ebad7387cc2
PaulMendieta28 <mendietarios@gmail.com>
2022-07-06 14:39:00 -0600

    Funcionalidad Agregar, Completar y Eliminar

--- ejercicio-combinado/src/components/task-form.jsx
+++ ejercicio-combinado/src/components/task-form.jsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import '../style/task-form.css';
+import PropTypes from "prop-types";
+import { v4 as uuidv4 } from 'uuid';
 
 const TaskForm = ({ onSubmit }) => {
+  const [title, setTitle] = useState("");
+
+  const handleChange = ({ target }) => setTitle(target.value);
+
+  const handleSendTask = e => {
+    e.preventDefault();
+
+    const newTask = {
+      id: uuidv4(),
+      text: title?.trim(),
+      complete: false,
+    };
+
+    if(typeof onSubmit === "function")
+      onSubmit(newTask);
+  };
+
   return (
     <form
       className="task-form"
+      onSubmit={ handleSendTask }
     >
       <input
         className="task-input"
         type='text'
         placeholder='Escribe una tarea'
         name='title'
+        value={title}
+        onChange={ handleChange }
       />
       <button className="task-button">
         Agregar tarea
       </button>
     </form>
   );
+};
+
+TaskForm.propTypes = {
+  onSubmit: PropTypes.func
+};
+
+TaskForm.defaultProps = {
+  onSubmit: null
 };
 
 export default TaskForm;
--- ejercicio-combinado/src/components/task-list.jsx
+++ ejercicio-combinado/src/components/task-list.jsx
@@ -5,16 +5,45 @@ import '../style/task-list.css';
 
 function ListTask() {
   const [tasks, setTasks] = useState([]);
-  
+  console.log('tasks', tasks)
+
+  const addTask = task => {
+    console.log(task);
+    if(task?.text){
+      const updatedTasks = [task, ...tasks];
+
+      setTasks(updatedTasks);
+    }
+  };
+
+  const deleteTask = id => {
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const completeTask = id => {
+    const updatedTasks = [...tasks];
+    const index = updatedTasks.findIndex(updatedTask => updatedTask.id === id);
+    const modifiedTask = updatedTasks[index];
+
+    updatedTasks[index] = { ...modifiedTask, complete: !modifiedTask.complete }
+
+    setTasks(updatedTasks);
+  }
+
   return (
     <>
-      <TaskForm />
+      <TaskForm onSubmit={ addTask } />
       <div className='task-container-list'>
         {
           tasks.map((task) => 
             <Task 
+              key={ task.id }
+              id={ task.id }
               text={ task.text }
               complete={ task.complete }
+              deleteTask={ deleteTask }
+              completeTask={ completeTask }
             />
           )
         }
